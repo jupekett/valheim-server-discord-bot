cb97d7393101c91ecd5575ebeaa24dd3047806e1
jupekett <jupekett@student.jyu.fi>
2021-12-15 18:44:25 +0200

    Clean up use of env variables

--- steamUsers.js
+++ steamUsers.js
@@ -8,7 +8,9 @@ dotenv.config();
 const steamUsers = {};
 const userCount = 4;
 for (let i = 1; i <= userCount; i++) {
-  steamUsers[process.env[`STEAM_ID${i}`]] = process.env[`STEAM_NAME${i}`];
+  const id = process.env[`STEAM_ID${i}`];
+  const name = process.env[`STEAM_NAME${i}`];
+  steamUsers[id] = name;
 }
 
 export { steamUsers };
