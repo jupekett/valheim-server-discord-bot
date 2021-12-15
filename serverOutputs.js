b8b14d0f65bd92e3623c5b4ee09451cee501eb95
jupekett <jupekett@student.jyu.fi>
2021-12-15 18:30:33 +0200

    Disable some messages to lower noise

--- serverOutputs.js
+++ serverOutputs.js
@@ -1,12 +1,12 @@
 // Enumerates relevant server output messages that should be handled in the bot script.
-// TODO: muuta stringiksi? Helpompi vertailla yms.
+// You can enable/disable the listened messages
 export const serverOutputs = {
-  SERVER_STARTING: /^Starting server/,
-  VALHEIM_VERSION: /Valheim version:(\d+.\d+.\d+)/,
-  WORLD_NAME: /Get create world ([\w ]+)\n/, 
+  // SERVER_STARTING: /^Starting server/,
+  // VALHEIM_VERSION: /Valheim version:(\d+.\d+.\d+)/,
+  // WORLD_NAME: /Get create world ([\w ]+)\n/, 
   SERVER_READY: /Load world/, // Last unique message when opening server
   PLAYER_JOINED: /Got handshake from client (\d+)/, // ..steamID
-  CHARACTER_NAME: /Got character ZDOID from ([\w ]+) :/, 
-  PLAYER_LEFT: /Closing socket (\d+)/, // ..steamID
+  // CHARACTER_NAME: /Got character ZDOID from ([\w ]+) :/, 
+  PLAYER_LEFT: /Closing socket (\d+)/, // ..steamID. FIXME: server doesn't always output this when player leaves.
   SERVER_CLOSING: /ZNet Shutdown/,
 };
