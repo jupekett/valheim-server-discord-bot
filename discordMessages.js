ea3f2464d53842b3374ed048946b12b5fb550764
Juho Kettunen <55327664+jupekett@users.noreply.github.com>
2021-12-15 21:54:41 +0200

    Update comment

--- discordMessages.js
+++ discordMessages.js
@@ -1,5 +1,4 @@
-// Enumerates relevant server output messages that should be handled in the bot script.
-// TODO: muuta stringiksi? Helpompi vertailla yms.
+// Discord messages that the bot will send based on server output.
 export const discordMessages = {
   SERVER_STARTING: "Serveri käynnistyy.",
   VALHEIM_VERSION: "Käytettävä Valheim-versio: ",
