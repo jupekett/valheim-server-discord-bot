2a56bdd1645c62f8ca388e57ced7501208a575a7
jupekett <jupekett@student.jyu.fi>
2021-12-21 17:21:34 +0200

    Fix single character join sequence buffering

--- src/buffers/joinSequenceBuffer.js
+++ src/buffers/joinSequenceBuffer.js
@@ -5,51 +5,51 @@ import {
 import { sendMessage } from "../bot.js";
 
 const JOIN_SEQUENCE_KEYS = getSequenceKeys("PLAYER_JOIN_SEQUENCE");
+const JOIN_BUFFERS = [];
 
-const joinBuffers = [];
-pushNewJoinBuffer();
+function bufferPlayerJoinMessage(channel, message, key) {
+  if (key === "PLAYER_JOINED") {
+    handlePlayerJoinMessage(message);
+    return;
+  } else if (key === "CHARACTER_NAME") {
+    handleCharacterMessage(channel, message);
+  } else {
+    console.warn("Wrong key for buffering player join messages: ", key);
+  }
+}
+
+function handlePlayerJoinMessage(message) {
+  pushNewJoinBuffer();
+  const buffer = getLatestJoinBuffer();
+  buffer["PLAYER_JOINED"] = message;
+}
 
 function pushNewJoinBuffer() {
   const joinSequenceBuffer = getEmptyJoinBuffer();
-  joinBuffers.push(joinSequenceBuffer);
+  JOIN_BUFFERS.push(joinSequenceBuffer);
 }
 
 function getEmptyJoinBuffer() {
   return getEmptyBufferWithSequenceKeys(JOIN_SEQUENCE_KEYS);
 }
 
-function bufferPlayerJoinMessage(channel, message, key) {
-  for (let buffer of joinBuffers) {
+function getLatestJoinBuffer() {
+  return JOIN_BUFFERS[JOIN_BUFFERS.length - 1];
+}
+
+function handleCharacterMessage(channel, message) {
+  for (let buffer of JOIN_BUFFERS) {
     const playerMessage = buffer["PLAYER_JOINED"];
     const characterMessage = buffer["CHARACTER_NAME"];
-    if (key === "PLAYER_JOINED" && !playerMessage) {
-      buffer["PLAYER_JOINED"] = message;
-    } else if (key === "PLAYER_JOINED" && playerMessage) {
-      // player join message should be buffered, but there is already a message there.
-      pushNewJoinBuffer(); // FIXME ei saa muuttaa iteroitavaa taulukkoa!
-      // TODO add the playerMessage to the fresh buffer
-    } else if (key === "CHARACTER_NAME" && !characterMessage && playerMessage) {
-      // matching player and character messages
-      // (with the exception of nested join sequences: join1-join2-character2-character1)
+    if (playerMessage && !characterMessage) {
+      // assuming sequential complete logins (join1-character1-join2-character2)
+      // TODO handle nested login sequences (join1-join2-character2-character1)
       buffer["CHARACTER_NAME"] = message;
       sendPlayerJoinMessage(channel, buffer);
       buffer = getEmptyJoinBuffer();
       return;
-    } else if (key == "CHARACTER_NAME" && characterMessage) {
-      // Character name message should be buffered, but there is already a message there.
-      // This shouldn't happen often, because buffer is emptied every time a login finishes.
-      console.warn(
-        "tried to buffer character name message, but buffer wasn't empty there"
-      );
-      console.warn(
-        "player join buffer wasn't properly emptied, or nested login message sequences happened"
-      );
     }
   }
-}
-
-function getLatestJoinBuffer() {
-  return joinBuffers[joinBuffers.length - 1];
 }
 
 function sendPlayerJoinMessage(channel, buffer) {
