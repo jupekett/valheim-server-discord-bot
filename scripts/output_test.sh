0d1eea340f72288305a375051dc22798a285e2a6
jupekett <jupekett@student.jyu.fi>
2021-12-21 18:35:46 +0200

    Add login sequences to output test script

--- scripts/output_test.sh
+++ scripts/output_test.sh
@@ -11,13 +11,58 @@ messages=(
     "12/15/2021 12:17:31: Load world TestWorld
 
 (Filename: ./Runtime/Export/Debug/Debug.bindings.h Line: 39)"
-    "12/15/2021 12:20:04: Got handshake from client 12345678901234567
+    "=====================================================
+TEST SEQUENCE: single login and logout
+====================================================="
+    "12/15/2021 12:20:04: Got handshake from client $MOCK_ID1
 
 (Filename: ./Runtime/Export/Debug/Debug.bindings.h Line: 39)"
-    "12/15/2021 12:20:37: Got character ZDOID from TestCharacter : -43114831:1
+    "12/15/2021 12:20:37: Got character ZDOID from $MOCK_CHARACTER1 : -43114831:1
 
 (Filename: ./Runtime/Export/Debug/Debug.bindings.h Line: 39)"
-    "12/15/2021 12:20:43: Closing socket 12345678901234567
+    "12/15/2021 12:20:43: Closing socket $MOCK_ID1
+
+(Filename: ./Runtime/Export/Debug/Debug.bindings.h Line: 39)"
+    "=====================================================
+TEST SEQUENCE: interweaved logins (join1-join2-character1-character2)
+====================================================="
+    "12/15/2021 12:20:04: Got handshake from client $MOCK_ID2
+
+(Filename: ./Runtime/Export/Debug/Debug.bindings.h Line: 39)"
+    "12/15/2021 12:20:04: Got handshake from client $MOCK_ID3
+
+(Filename: ./Runtime/Export/Debug/Debug.bindings.h Line: 39)"
+    "12/15/2021 12:20:37: Got character ZDOID from $MOCK_CHARACTER2 : -43114831:1
+
+(Filename: ./Runtime/Export/Debug/Debug.bindings.h Line: 39)"
+    "12/15/2021 12:20:37: Got character ZDOID from $MOCK_CHARACTER3 : -43114831:1
+
+(Filename: ./Runtime/Export/Debug/Debug.bindings.h Line: 39)"
+    "12/15/2021 12:20:43: Closing socket $MOCK_ID2
+
+(Filename: ./Runtime/Export/Debug/Debug.bindings.h Line: 39)"
+    "12/15/2021 12:20:43: Closing socket $MOCK_ID3
+
+(Filename: ./Runtime/Export/Debug/Debug.bindings.h Line: 39)"
+    "=====================================================
+TEST SEQUENCE: nested logins (join1-join2-character2-character1)
+====================================================="
+    "12/15/2021 12:20:04: Got handshake from client $MOCK_ID1
+
+(Filename: ./Runtime/Export/Debug/Debug.bindings.h Line: 39)"
+    "12/15/2021 12:20:04: Got handshake from client $MOCK_ID4
+
+(Filename: ./Runtime/Export/Debug/Debug.bindings.h Line: 39)"
+    "12/15/2021 12:20:37: Got character ZDOID from $MOCK_CHARACTER4 : -43114831:1
+
+(Filename: ./Runtime/Export/Debug/Debug.bindings.h Line: 39)"
+    "12/15/2021 12:20:37: Got character ZDOID from $MOCK_CHARACTER1 : -43114831:1
+
+(Filename: ./Runtime/Export/Debug/Debug.bindings.h Line: 39)"
+    "12/15/2021 12:20:43: Closing socket $MOCK_ID1
+
+(Filename: ./Runtime/Export/Debug/Debug.bindings.h Line: 39)"
+    "12/15/2021 12:20:43: Closing socket $MOCK_ID4
 
 (Filename: ./Runtime/Export/Debug/Debug.bindings.h Line: 39)"
     "12/15/2021 12:24:15: ZNet Shutdown
--- src/buffers/joinSequenceBuffer.js
+++ src/buffers/joinSequenceBuffer.js
@@ -41,14 +41,15 @@ function handleCharacterMessage(channel, message) {
   for (let buffer of JOIN_BUFFERS) {
     const playerMessage = buffer["PLAYER_JOINED"];
     const characterMessage = buffer["CHARACTER_NAME"];
+    // handle sequential complete logins (join1-character1-join2-character2)
+    // Also works with interweaved logins (join1-join2-character1-character2)
     if (playerMessage && !characterMessage) {
-      // assuming sequential complete logins (join1-character1-join2-character2)
-      // TODO handle nested login sequences (join1-join2-character2-character1)
       buffer["CHARACTER_NAME"] = message;
       sendPlayerJoinMessage(channel, buffer);
       buffer = getEmptyJoinBuffer();
       return;
     }
+    // TODO handle nested logins (join1-join2-character2-character1)
   }
 }
 
